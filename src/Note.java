6ba752d60ae598ee57a9aa4769e90a8ef4fae05b
bogdan <bogdan.calburean2004@gmail.com>
2025-10-20 20:52:28 +0300

    1.3,1.4

--- src/Note.java
+++ src/Note.java
@@ -1,6 +1,10 @@
 public class Note {
     private int note;
 
+    public int getNote(){
+        return note;
+    }
+
     public int [] SchlechteNote(int [] Noten) {
         int count = 0;
         for (int i = 0; i < Noten.length; i++) {
@@ -27,5 +31,30 @@ public class Note {
             sum += Noten[i];
         }
         return sum / Noten.length;
+    }
+
+    public int [] AufrundeNote(int [] Noten) {
+        int [] aufrundeNoten = new int[Noten.length];
+        for (int i = 0; i < Noten.length; i++) {
+            int x = Noten[i] / 5;
+            if (Noten[i] >= 38) {
+                x = x+1;
+                x = x * 5;
+                if (x - Noten[i] < 3) {
+                    aufrundeNoten[i] = x;
+                }
+            }
+        }
+        return aufrundeNoten;
+    }
+
+    public  int MaxAufgerundeNote(int [] Noten){
+        int max = 0;
+        for (int i = 0; i < Noten.length; i++) {
+            if (Noten[i] > max) {
+                max = Noten[i];
+            }
+        }
+        return max;
     }
 }
